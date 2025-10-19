a1fb617bd8e91e73a708953399f35701a3839e2b
Josh Levine <bigjosh@users.noreply.github.com>
2025-10-18 23:28:59 -0400

    Update paths in start_incremental_builder.bat

--- start_incremental_builder.bat
+++ start_incremental_builder.bat
@@ -1,7 +1,7 @@
 echo ** Starting the world builder. It will run every 60 seconds to update any new tiles.
-echo ** Note that you MUST init with 'python incremental_build.py --parcels-dir D:\Github\cwandt-pocketfiche-site\testing-data-dir\parcels --output-dir D:\Github\cwandt-pocketfiche-site\docs\world --init'
+echo ** Note that you MUST init with 'python incremental_build.py --parcels-dir %PF_DATA_DIR%\parcels --output-dir docs\world --init'
 :loop
-python incremental_build.py --parcels-dir D:\Github\cwandt-pocketfiche-site\testing-data-dir\parcels --output-dir D:\Github\cwandt-pocketfiche-site\docs\world
+python incremental_build.py --parcels-dir %PF_DATA_DIR%\parcels --output-dir docs\world
 @echo "Repeat? (Y=repeats, N=Quit, wait 60 seconds to auto repeat)"
 choice /C YN /T 60 /D Y
 if not errorlevel 2 goto :loop
